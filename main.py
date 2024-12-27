c9c126a8e303adb09d1e6f2ba8d86145217a6b06
Morris <morrisshevchenko@gmail.com>
2024-12-27 18:57:34 +0700

    0.2

--- main.py
+++ main.py
@@ -39,9 +39,7 @@ def callback_1(callback): #Ферментация
         keyboard_TeaPuer.add(TeaPuer_palace, TeaPuer_dark, TeaPuer_light)
         bot.send_message(callback.from_user.id, "Пуэр", reply_markup=keyboard_TeaPuer)
 
-@bot.callback_query_handler(func= lambda callback: True)
-def callback_2(callback): #4 вида ферментации чая
-    if callback.data == 'Strongly':
+    elif callback.data == 'Strongly':
         bot.send_message(callback.from_user.id, "Чёрный чай: Самый популярный в России и в Европе. Производится путём сильной ферментации (окисления). Как правило, имеет терпкий насыщенный вкус.")
     elif callback.data == 'Weakly':
         keyboard_white_and_yellow = types.InlineKeyboardMarkup()
@@ -54,18 +52,12 @@ def callback_2(callback): #4 вида ферментации чая
     elif callback.data == 'Semi':
         bot.send_message(callback.from_user.id, 'Улун чай: Полуферментированный чай, занимающий среднее положение между чёрным и зелёным. По степени окисления делится на сильноферментированные (ближе к чёрному чаю) и слабоферментированные (ближе к зелёному) сорта. Улуны дают настой от светло-жёлтого до светло-коричневого цвета, вкус насыщенный и яркий.')
 
-
-@bot.callback_query_handler(func=lambda callback: True)
-def callback_2_1(callback): # w y
-    if callback.data == 'White':
+    elif callback.data == 'White':
         bot.send_message(callback.from_user.id, 'Белый чай: Слабоферментированный чай, содержащий типсы (чайные почки). Особенно популярен в Китае, считается максимально полезным. Белый чай имеет светло-жёлтый цвет и нежный аромат.')
     elif callback.data == 'Yellow':
         bot.send_message(callback.from_user.id, 'Жёлтый чай: Слабоферментированный чай из сырья высокого качества. По степени окисления находится между белым и зелёным. Считается очень редким и чаще всего может быть приобретён только в Китае.')
 
-
-@bot.callback_query_handler(func= lambda callback: True)
-def callback_3(callback): #Пуэр
-    if callback.data == 'Light':
+    elif callback.data == 'Light':
         bot.send_message(callback.from_user.id,'Шэн пуэр (светлый): Переводится как «сырой, свежий». При обработке не проходит процесс ферментации, а дозревает потом. С каждым годом шэн темнеет, его вкус становится более мягким и цельным.')
     elif callback.data == 'Dark':
         bot.send_message(callback.from_user.id,'Шу пуэр (тёмный): При обработке листья подвергают ускоренной ферментации, которая имитирует старение чая в более короткие сроки — в течение 45–65 дней. У молодого чая вкус резкий и древесный, а со временем развивается, становится сложным и глубоким.')
